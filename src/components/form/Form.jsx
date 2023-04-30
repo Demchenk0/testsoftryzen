aecc8dc08b30df747ea273e7ddb95209810f0cd7
Demchenk0 <santadyr2016@gmail.com>
2023-04-30 21:05:53 +0300

    asdasasdasd

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from './Form.module.scss';
-// import { useForm } from 'react-hook-form';
-// import error from '../../aseets/images/form/worning.svg';
+import { useForm } from 'react-hook-form';
+import error from '../../aseets/images/form/worning.svg';
 // import queryString from 'query-string';
+
+// import { encode } from 'querystring';
 
 
 const Form = () => {
@@ -12,37 +14,65 @@ const Form = () => {
 	// 	formState: { errors },
 	// } = useForm();
 
+  const { register, handleSubmit, formState: { errors } } = useForm();
   
+  const onSubmit = (encode, reset) => (data, e) => {
+    e.preventDefault();
+    // const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': 'contact',
+        body: data,
+      }),
+    })
+      .then(response => {
+        reset();                
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 	return (
 		<section name="contact" className={styles.contact}>
 			<div className={styles.box}>
 				<div className={styles.img}></div>
 				<div className={styles.boxForm}>
 					<p className={styles.callback}>Request Callback</p>
+          <form
+    name="contact"
+    method="POST"
+    data-netlify="true"
+    onSubmit={handleSubmit(onSubmit)}
+  >
+    <input type="hidden" name="form-name" value="contact" />
+    <input
+      name="name"
+      className={styles.input}
+      type="text"
+      placeholder="Enter your name"
+      {...register('name')}
+    />
+    <input
+      name="email"
+      className={styles.input}
+      type="email"
+      placeholder="Enter email*"
+      {...register('email', { required: true })}
+    />
+    {errors.email && (
+      <div className={styles.error}>
+        <img src={error} alt="error" />
+        <p>This is a required field</p>
+      </div>
+    )}
+    <button className={styles.button} type="submit">
+      Send
+    </button>
+  </form>
 
-
-          <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-
-<input type="hidden" name="form-name" value="contact"></input>
 					{/* <form
 						name="contact"
 						className={styles.form}
