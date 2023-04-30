3f8cebbf0594eccf796f97e97e827d348cdfeaf7
Demchenk0 <santadyr2016@gmail.com>
2023-04-30 22:45:21 +0300

    updade

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,101 +1,57 @@
 
 import React from 'react';
 import styles from './Form.module.scss';
-// import { useForm } from 'react-hook-form';
-// import error from '../../aseets/images/form/worning.svg';
+import { useForm } from 'react-hook-form';
+import error from '../../aseets/images/form/worning.svg';
 
 
-// const encode = data => {
-//   return Object.keys(data)
-//     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-//     .join("&");
-// };
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
 const Form = () => {
-	// const {
-  //   register,
-  //   handleSubmit,
-  //   reset,
-  //   formState: { errors },
-  // } = useForm();
+	const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  // const onSubmit = (data, e) => {
-  //   e.preventDefault();
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({
-  //       "form-name": "contact",
-  //       name: data.name,
-  //       email: data.email,
-  //     }),
-  //   })
-  //     .then(response => {
-  //       reset();
-  //       console.log(response);
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // };
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": "contact",
+        name: data.name,
+        email: data.email,
+      }),
+    })
+      .then(response => {
+        reset();
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 	return (
 		<section name="contact" className={styles.contact}>
 			<div className={styles.box}>
 				<div className={styles.img}></div>
 				<div className={styles.boxForm}>
 					<p className={styles.callback}>Request Callback</p>
-          <form name="contact" className={styles.form} netlify>
-          <input type="hidden" name="form-name" value="contact"></input>
-  <p>
-    <label>Name <input className={styles.input} type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input className={styles.input} type="email" name="email" /></label>
-  </p>
-  <p>
-    <button className={styles.button} type="submit">Send</button>
-  </p>
-</form>
-					{/* <form
+   
+					<form
 						name="contact"
 						className={styles.form}
 						method="POST"
 						data-netlify="true"
 						data-netlify-honeypot="bot-field"
             onSubmit={handleSubmit(onSubmit)}
-
-
-						// onSubmit={handleSubmit(data => {
-						// 	const encodedData = queryString.stringify({
-						// 		'form-name': 'contact',
-						// 		...data,
-						// 	});
-						// 	fetch('/', {
-						// 		method: 'POST',
-						// 		headers: {
-						// 			'Content-Type': 'application/x-www-form-urlencoded',
-						// 		},
-						// 		body: new URLSearchParams(encodedData).toString(),
-						// 	})
-						// 		.then(() => console.log('Form successfully submitted'))
-						// 		.catch(error => alert(error));
-						// })}
-
-
-
-						// onSubmit={handleSubmit(data => {
-						// 	const formData = new FormData();
-						// 	formData.append('name', data.name);
-						// 	formData.append('email', data.email);
-
-						// 	fetch('/', {
-						// 		method: 'POST',
-						// 		headers: {
-						// 			'Content-Type': 'application/x-www-form-urlencoded',
-						// 		},
-						// 		body: new URLSearchParams(formData).toString(),
-						// 	}).then(() => alert('/thank-you/'));
-						// })}
 					>
 						<input type="hidden" name="form-name" value="contact" />
 						<input
@@ -120,8 +76,9 @@ const Form = () => {
 						)}
 						<button className={styles.button} type="submit">
 							Send
+              
 						</button>
-					</form> */}
+					</form>
 				</div>
 			</div>
 		</section>
