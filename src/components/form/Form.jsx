14a824797a6ff617b1c13eb209b5bc9cba8a7a52
Demchenk0 <santadyr2016@gmail.com>
2023-04-30 21:13:51 +0300

    asdasasdasdasd

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,78 +2,27 @@ import React from 'react';
 import styles from './Form.module.scss';
 import { useForm } from 'react-hook-form';
 import error from '../../aseets/images/form/worning.svg';
-// import queryString from 'query-string';
+import queryString from 'query-string';
 
-// import { encode } from 'querystring';
 
 
 const Form = () => {
-	// const {
-	// 	register,
-	// 	handleSubmit,
-	// 	formState: { errors },
-	// } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 
-  const { register, handleSubmit, formState: { errors } } = useForm();
   
-  const onSubmit = (encode, reset) => (data, e) => {
-    e.preventDefault();
-    // const form = e.target;
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': 'contact',
-        body: data,
-      }),
-    })
-      .then(response => {
-        reset();                
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
 	return (
 		<section name="contact" className={styles.contact}>
 			<div className={styles.box}>
 				<div className={styles.img}></div>
 				<div className={styles.boxForm}>
 					<p className={styles.callback}>Request Callback</p>
-          <form
-    name="contact"
-    method="POST"
-    data-netlify="true"
-    onSubmit={handleSubmit(onSubmit)}
-  >
-    <input type="hidden" name="form-name" value="contact" />
-    <input
-      name="name"
-      className={styles.input}
-      type="text"
-      placeholder="Enter your name"
-      {...register('name')}
-    />
-    <input
-      name="email"
-      className={styles.input}
-      type="email"
-      placeholder="Enter email*"
-      {...register('email', { required: true })}
-    />
-    {errors.email && (
-      <div className={styles.error}>
-        <img src={error} alt="error" />
-        <p>This is a required field</p>
-      </div>
-    )}
-    <button className={styles.button} type="submit">
-      Send
-    </button>
-  </form>
+          
 
-					{/* <form
+					<form
 						name="contact"
 						className={styles.form}
 						method="POST"
@@ -85,15 +34,15 @@ const Form = () => {
         ...data
     });
 
-    fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: encodedData
-    }).then(() => alert('/thank-you/'));
-})}
-
-            
-           
+     
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(encodedData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+})}           
 						// onSubmit={handleSubmit(data => {
 						// 	const formData = new FormData();
 						// 	formData.append('name', data.name);
@@ -132,7 +81,7 @@ const Form = () => {
 						<button className={styles.button} type="submit">
 							Send
 						</button>
-					</form> */}
+					</form>
 				</div>
 			</div>
 		</section>
